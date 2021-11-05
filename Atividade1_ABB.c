dd77feeba76e633b8deb4eacfb8a9d8c43f65dff
Luiz Torre <luiztorre@id.uff.br>
2021-11-05 17:04:25 -0300

    Fazendo o verifica existencia

--- Atividade1_ABB.c
+++ Atividade1_ABB.c
@@ -13,6 +13,90 @@ void ImprimirOrdem(arvore *a);
 void ImprimirPosOrdem(arvore *a);
 void ImprimirLargura(arvore *a, int aux, int nivel);
 int CalcAltura (arvore *a);
+int VerificaExistencia(arvore *a, int valor_desejado);
+
+
+int main(){
+    FILE * arq;
+    int op;
+    int valor_desejado, verifica, altura,aux_balanceada, aux_cheia, num_no;
+    char nome_arquivo[50];
+    while (op!= 9)
+    {    
+    
+    printf("\n\nO que você deseja fazer? Digite: \n1- Para ler a arvore de um arquivo\n2-Para imprimir a arvore.\n3- Verificar se um elemento existe na arvore\n4-Imprimir o Nivel de Um no\n5-Imprimir as folhas menores que um valor\n6-Inserir um no X na arvore\n7-Remover um no X da arvore\n8-Sair\n");
+    scanf("%d",&op);
+
+    switch (op)
+    {
+    case 1:
+        puts("Informe o nome do arquivo a ser lido");
+        scanf("%s", nome_arquivo);
+        arq = fopen(nome_arquivo, "rt");
+        if (arq == NULL)
+        {
+            printf("Erro ao criar o arquivo\n");
+            return 1;
+        }
+        arvore *a ;
+        a = LerArvore(arq);
+        fclose(arq);
+        break;
+    
+    case 2:
+        printf("\nImpressão Pre ordem: \n");
+        ImprimirPreOrdem(a);
+        printf("\n\nImpressão em ordem: \n");
+        ImprimirOrdem(a);
+        printf("\n\nImpressão pós ordem: \n");
+        ImprimirPosOrdem(a);
+        altura = CalcAltura(a);
+        printf("\n\nImpressão em largura: \n");
+        for (int nivel = 0; nivel < altura; nivel++){
+            printf("Nivel %d : ", nivel);
+            ImprimirLargura(a, 0, nivel);
+            printf("\n"); 
+        }
+        break;
+    case 3:
+        puts("Informe o elemento que deseja saber existencia");
+        scanf("%d", &valor_desejado);
+        VerificaExistencia(a, valor_desejado);
+    case 9:
+        break;
+    default:
+        printf ("Valor invalido!\n");
+    }
+   
+    }
+    return 0;
+}
+
+arvore *LerArvore(FILE *arq){
+    char c;
+    int num;
+    arvore *a;
+    a = NULL;
+    fscanf(arq,"%c",&c);
+    fscanf(arq,"%d",&num);
+
+    if(num==-1){
+        fscanf(arq,"%c",&c);
+        return NULL;
+    }
+
+    else{
+        a = (arvore*)malloc(sizeof(arvore));
+        a->info= num;
+        a->esq = LerArvore(arq);
+        a->dir = LerArvore(arq);
+        fscanf(arq,"%c",&c);
+        
+
+        return a;
+
+    }
+}
 
 void ImprimirPreOrdem(arvore *a){
      if(a!=NULL){
@@ -68,81 +152,19 @@ int CalcAltura (arvore *a){
 	}
 }
 
+int VerificaExistencia(arvore *a, int valor_desejado){
+    if(valor_desejado>a->info){
+        printf("%d", valor_desejado);
 
-int main(){
-    FILE * arq;
-    int op;
-    int valor_desejado, verifica, altura,aux_balanceada, aux_cheia, num_no;
-    char nome_arquivo[50];
-    while (op!= 9)
-    {    
-    
-    printf("\n\nO que você deseja fazer? Digite: \n1- Para ler a arvore de um arquivo\n2-Para imprimir a arvore.\n3- Verificar se um elemento existe na arvore\n4-Imprimir o Nivel de Um no\n5-Imprimir as folhas menores que um valor\n6-Inserir um no X na arvore\n7-Remover um no X da arvore\n8-Sair\n");
-    scanf("%d",&op);
-
-    switch (op)
-    {
-    case 1:
-        puts("Informe o nome do arquivo a ser lido");
-        scanf("%s", nome_arquivo);
-        arq = fopen(nome_arquivo, "rt");
-        if (arq == NULL)
-        {
-            printf("Erro ao criar o arquivo\n");
-            return 1;
-        }
-        arvore *a ;
-        a = LerArvore(arq);
-        fclose(arq);
-        break;
-    
-    case 2:
-        printf("\nImpressão Pre ordem: \n");
-        ImprimirPreOrdem(a);
-        printf("\n\nImpressão em ordem: \n");
-        ImprimirOrdem(a);
-        printf("\n\nImpressão pós ordem: \n");
-        ImprimirPosOrdem(a);
-        altura = CalcAltura(a);
-        printf("\n\nImpressão em largura: \n");
-        for (int nivel = 0; nivel < altura; nivel++){
-            printf("Nivel %d : ", nivel);
-            ImprimirLargura(a, 0, nivel);
-            printf("\n"); 
-        }
-        break;
-    case 9:
-        break;
-    default:
-        printf ("Valor invalido!\n");
+        VerificaExistencia(a->dir,valor_desejado);
     }
-   
-    }
-    return 0;
-}
-
-arvore *LerArvore(FILE *arq){
-    char c;
-    int num;
-    arvore *a;
-    a = NULL;
-    fscanf(arq,"%c",&c);
-    fscanf(arq,"%d",&num);
-
-    if(num==-1){
-        fscanf(arq,"%c",&c);
-        return NULL;
-    }
-
-    else{
-        a = (arvore*)malloc(sizeof(arvore));
-        a->info= num;
-        a->esq = LerArvore(arq);
-        a->dir = LerArvore(arq);
-        fscanf(arq,"%c",&c);
-        
-
-        return a;
-
-    }
+    // if(valor_desejado<a->info){
+    //     VerificaExistencia(a->esq,valor_desejado);
+    // }
+    // if(valor_desejado == a->info){
+    //     return 1;
+    // }
+    // else{
+    //     return 0;
+    // }
 }
